972548c550559b208d086b26c1549cc7787383ce
AkshataKrishnaHegde <hegdeakshata478@gmail.com>
2025-02-13 11:43:40 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
-import Home from "./components/Home";
-import Cart from "./components/Cart";
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import { useState } from 'react';
 
 const App = () => {
-  const [cart, setCart] = useState<any[]>([]);
+  const [cart] = useState<any[]>([]);
 
   return (
-   
-      <Router>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/cart" element={<Cart cart={cart} />} />
-        </Routes>
-      </Router>
+    <Router basename="/demo-project"> {/* Add basename here */}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart cart={cart} />} />
+      </Routes>
+    </Router>
   );
 };
 
